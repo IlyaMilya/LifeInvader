bb9fa60d6a4d6270e9938e8a2f8a7fdb7a4bccd6
Ilya <milyavskyfamily@gmail.com>
2022-05-25 17:50:57 -0400

    uploading forms and logins

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const handleSignup = async () => {
+    let form = new FormData(document.querySelector('#signup-form'))
+    let req = await fetch('/signup', {
+  method: 'POST',
+  body: form
+
+    })
+
+  }
+
+  const handleLogin = async () => {
+    let form = new FormData(document.querySelector('#login-form'))
+    let req = await fetch('/login', {
+  method: 'POST',
+  body: form
+
+    })
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+  
+<div>
+      <h1>Sign up</h1>
+      <form action="/signup" method="POST" id="signup-form" onSubmit={(e) => {
+        e.preventDefault()
+        handleSignup()
+      }}>
+        <input type="text" name="user[username]"  placeholder= "Username"/>
+        <input type="text" name="user[first_name]"  placeholder= "First Name"/>
+        <input type="text" name="user[last_name]" placeholder="Last Name"/>
+        <input type="email" name="user[email]" placeholder="email"/>
+        <input type="password" name="user[password]" placeholder= "password"/>
+        <input type="submit" />
+      </form>
+    <hr/>
+    <h2> Login </h2>
+    <form action="/login" method="POST" id="login-form" onSubmit={handleLogin}>
+        <input type="text" name="user[email]" placeholder="email"/>
+        <input type="text" name="user[password]" placeholder="password"/>
+        <input type="submit" />
+   </form>
+   </div>
+  )
+  ;
 }
 
 export default App;
